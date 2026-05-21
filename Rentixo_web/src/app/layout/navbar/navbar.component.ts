fe20bfb5cac340ecc101d918d7a1dbadc5a5fcc0
Binoy kr <binoykrbar@gmail.com>
2026-05-22 00:35:40 +0530

    user frontend design changes

--- Rentixo_web/src/app/layout/navbar/navbar.component.ts
+++ Rentixo_web/src/app/layout/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass'
 })
 export class NavbarComponent {
-
+  isMobileMenuOpen = false;
 }

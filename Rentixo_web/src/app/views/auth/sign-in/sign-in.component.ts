5ccd3cf6599aa03333308946a22e4c720b9ca7a8
Binoy kr <binoykrbar@gmail.com>
2026-05-26 22:44:06 +0530

    Updated user Interface

--- Rentixo_web/src/app/views/auth/sign-in/sign-in.component.ts
+++ Rentixo_web/src/app/views/auth/sign-in/sign-in.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.sass'
 })
--- Rentixo_web/src/app/views/auth/sign-up/sign-up.component.ts
+++ Rentixo_web/src/app/views/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   imports: [
-    
+    RouterModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.sass'
